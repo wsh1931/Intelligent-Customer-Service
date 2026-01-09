fef1dd99061304a09e7a5bbbeb0f8526ad8a0389
wusihao <1931443283@qq.com>
2026-01-09 12:09:22 +0800

    完成转账机器人案例

--- rasa_calm/actions/actions.py
+++ rasa_calm/actions/actions.py
@@ -4,8 +4,9 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
-
 class ActionCheckSufficientFunds(Action):
+    """检查余额是否充足"""
+
     def name(self) -> Text:
         return "action_check_sufficient_funds"
 
@@ -15,9 +16,29 @@ class ActionCheckSufficientFunds(Action):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        # hard-coded balance for tutorial purposes. in production this
-        # would be retrieved from a database or an API
-        balance = 1000
+        events = []
+        # 获取槽位中的余额和转账金额
+        balance = tracker.get_slot("balance")
         transfer_amount = tracker.get_slot("amount")
+        # 检查余额是否充足
         has_sufficient_funds = transfer_amount <= balance
-        return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
+        # 填充余额是否充足槽位
+        events.append(SlotSet("has_sufficient_funds", has_sufficient_funds))
+        return events
+
+class ActionTransfer(Action):
+    """转账"""
+    def name(self) -> Text:
+        return "action_transfer"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        # 获取槽位中的余额和转账金额
+        balance = tracker.get_slot("balance")
+        transfer_amount = tracker.get_slot("amount")
+        # 扣除转账金额，并填充到余额槽位
+        return [SlotSet("balance", balance - transfer_amount)]
