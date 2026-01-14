9b4adf87fe88795f47892555b6ed96c4a57c97ad
wusihao <1931443283@qq.com>
2026-01-14 22:50:20 +0800

    修改订单收货信息

--- rasa_ecs/actions/action_order.py
+++ rasa_ecs/actions/action_order.py
@@ -103,6 +103,13 @@ class AskOrderId(Action):
                         OrderInfo.complete_time > datetime.now() - timedelta(days=3),
                     ),
                 )
+            case "action_ask_order_id_before_delivered":
+                 # 查询已签收之前状态的订单
+                return and_(
+                    OrderInfo.user_id == user_id,
+                    OrderInfo.order_status != "已取消",
+                    OrderStatus.status_code <= 320,
+                )
             
 class GetOrderDetail(Action):
     """获取订单详情"""
@@ -221,4 +228,214 @@ class GetOrderDetail(Action):
                     f"  - {postsale.logistics[0].logistics_tracking.splitlines()[-1]}"
                 )
             dispatcher.utter_message(text="\n".join(message))
+        return []
+    
+class AskReceiveId(Action):
+    """重新选择现有的收货信息，或修改并新建收货信息"""
+
+    def name(self) -> str:
+        return "action_ask_receive_id"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[Text, Any]]:
+        user_id = tracker.get_slot("user_id")
+        order_id = tracker.get_slot("order_id")
+        # 查询用户现有收货信息
+        with SessionLocal() as session:
+            receive_infos = session.query(ReceiveInfo).filter_by(user_id=user_id).all()
+            current_receive_info = (
+                session.query(OrderInfo).filter_by(order_id=order_id).first().receive
+            )
+        buttons = []
+        for receive_info in receive_infos:
+            buttons.append(
+                {
+                    "title": f"收货人姓名：{receive_info.receiver_name} - \
+                    联系电话：{receive_info.receiver_phone} - \
+                    收货地址：{receive_info.receive_province}\
+                    {receive_info.receive_city}\
+                    {receive_info.receive_district}\
+                    {receive_info.receive_street_address}",
+                    "payload": f"/SetSlots(receive_id={receive_info.receive_id})",
+                }
+            )
+        buttons.extend(
+            [
+                {
+                    "title": "修改并新建收货信息",
+                    "payload": f"/SetSlots(receive_id=modify)",
+                },
+                {"title": "取消", "payload": f"/SetSlots(receive_id=false)"},
+            ]
+        )
+        dispatcher.utter_message(
+            text="请选择现有的收货信息，或修改并新建收货信息", buttons=buttons
+        )
+        return [
+            SlotSet("receiver_name", current_receive_info.receiver_name),
+            SlotSet("receiver_phone", current_receive_info.receiver_phone),
+            SlotSet("receive_province", current_receive_info.receive_province),
+            SlotSet("receive_city", current_receive_info.receive_city),
+            SlotSet("receive_district", current_receive_info.receive_district),
+            SlotSet(
+                "receive_street_address", current_receive_info.receive_street_address
+            ),
+        ]
+
+class AskReceiveProvince(Action):
+    """询问收货省"""
+
+    def name(self) -> str:
+        return "action_ask_receive_province"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[Text, Any]]:
+        with SessionLocal() as session:
+            provinces = session.query(Region.province).distinct().all()
+        buttons = [
+            {
+                "title": province[0],
+                "payload": f"/SetSlots(receive_province={province[0]})",
+            }
+            for province in provinces
+        ]
+        dispatcher.utter_message(text="请选择省份", buttons=buttons)
+        return []
+
+class AskReceiveCity(Action):
+    """询问收货市"""
+
+    def name(self) -> str:
+        return "action_ask_receive_city"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[Text, Any]]:
+        receive_province = tracker.get_slot("receive_province")
+        with SessionLocal() as session:
+            cities = (
+                session.query(Region.city)
+                .filter(Region.province == receive_province)
+                .distinct()
+                .all()
+            )
+        buttons = [
+            {"title": city[0], "payload": f"/SetSlots(receive_city={city[0]})"}
+            for city in cities
+        ]
+        dispatcher.utter_message(text="请选择城市", buttons=buttons)
+        return []
+
+class AskReceiveDistrict(Action):
+    """询问收货区"""
+
+    def name(self) -> str:
+        return "action_ask_receive_district"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[Text, Any]]:
+        receive_city = tracker.get_slot("receive_city")
+        with SessionLocal() as session:
+            districts = (
+                session.query(Region.district)
+                .filter(Region.city == receive_city)
+                .distinct()
+                .all()
+            )
+        buttons = [
+            {
+                "title": district[0],
+                "payload": f"/SetSlots(receive_district={district[0]})",
+            }
+            for district in districts
+        ]
+        dispatcher.utter_message(text="请选择城区", buttons=buttons)
+        return []
+
+class AskSetReceiveInfo(Action):
+    """设置收货信息"""
+
+    def name(self) -> str:
+        return "action_ask_set_receive_info"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[Text, Any]]:
+        receive_id = tracker.get_slot("receive_id")
+        set_receive_info = tracker.get_slot("set_receive_info")
+        # 获取收货信息
+        if receive_id in ("modify", "modified"):
+            receive_info = ReceiveInfo(
+                receive_id="rec" + uuid4().hex[:16],
+                user_id=tracker.get_slot("user_id"),
+                receiver_name=tracker.get_slot("receiver_name"),
+                receiver_phone=tracker.get_slot("receiver_phone"),
+                receive_province=tracker.get_slot("receive_province"),
+                receive_city=tracker.get_slot("receive_city"),
+                receive_district=tracker.get_slot("receive_district"),
+                receive_street_address=tracker.get_slot("receive_street_address"),
+            )
+        else:
+            with SessionLocal() as session:
+                receive_info = (
+                    session.query(ReceiveInfo).filter_by(receive_id=receive_id).first()
+                )
+        # 如果确认修改，进行修改
+        if set_receive_info:
+            order_id = tracker.get_slot("order_id")
+            with SessionLocal() as session:
+                order_info = (
+                    session.query(OrderInfo).filter_by(order_id=order_id).first()
+                )
+                # 如果没有使用已有的收货信息，向数据库中添加新的收货信息
+                if receive_id in ("modify", "modified"):
+                    # 查询收货信息是否已存在
+                    old_receive_info = (
+                        session.query(ReceiveInfo)
+                        .filter(
+                            ReceiveInfo.user_id == receive_info.user_id,
+                            ReceiveInfo.receiver_name == receive_info.receiver_name,
+                            ReceiveInfo.receiver_phone == receive_info.receiver_phone,
+                            ReceiveInfo.receive_province
+                            == receive_info.receive_province,
+                            ReceiveInfo.receive_city == receive_info.receive_city,
+                            ReceiveInfo.receive_district
+                            == receive_info.receive_district,
+                            ReceiveInfo.receive_street_address
+                            == receive_info.receive_street_address,
+                        )
+                        .first()
+                    )
+                    if old_receive_info:
+                        receive_info = old_receive_info
+                        dispatcher.utter_message(
+                            text="此收货信息已存在，将不再重复添加"
+                        )
+                    else:
+                        session.add(receive_info)
+                        session.flush()
+                order_info.receive_id = receive_info.receive_id
+                session.commit()
+            dispatcher.utter_message(text="订单收货信息已修改")
+        # 展示收货信息，询问是否确认修改
+        else:
+            message = [
+                f"- 收货人姓名：{receive_info.receiver_name}",
+                f"- 联系电话：{receive_info.receiver_phone}",
+                f"- 收货省份：{receive_info.receive_province}",
+                f"- 收货城市：{receive_info.receive_city}",
+                f"- 收货城区：{receive_info.receive_district}",
+                f"- 收货地址：{receive_info.receive_street_address}",
+            ]
+            dispatcher.utter_message(text="\n".join(message))
+            dispatcher.utter_message(
+                text="是否确认修改？",
+                buttons=[
+                    {"title": "确认", "payload": "/SetSlots(set_receive_info=true)"},
+                    {"title": "取消", "payload": "/SetSlots(set_receive_info=false)"},
+                ],
+            )
         return []
